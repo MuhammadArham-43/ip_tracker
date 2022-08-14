6daccae798e0a2a0ffbf82ec82cc6afebe64d2fd
Muhammad Arham <iqbalarham43@gmail.com>
2022-08-14 17:49:36 +0500

    Added Github Pages Deployment

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
     }
 
     const res = await axios.get(
-			`/country,city?apiKey=at_BaYbdhnJkiKQP1hGUZkcp5Osb4k4c&ipAddress=${ip}`,
+			`https://geo.ipify.org/api/v2/country,city?apiKey=at_BaYbdhnJkiKQP1hGUZkcp5Osb4k4c&ipAddress=${ip}`,
 			{
 				headers: {
 					"Allow-Control-Access-Origin": "*",
